e212475089db2a00b430011660b786fc8a8922a6
ChoeChangWon <qwefgh90@naver.com>
2016-03-25 18:44:35 +0900

    front-end complete and jquery dotdotdot test

--- src/main/webapp/staticfiles/js/searchController.js
+++ src/main/webapp/staticfiles/js/searchController.js
@@ -4,30 +4,74 @@
 
 var app = angular.module('searchModule', ['searchServiceModule']);
 
-app.controller('resultController', ['searchService',
-function(searchService) {
+app.controller('resultController', ['searchService', '$scope', '$timeout', '$window',
+function(searchService, $timeout, $scope, $window) {
+
+	$(".ellipsis").dotdotdot({
+
+		ellipsis	: '... ',
+		watch: "window",
+		height		: '100px',
+		wrap		: 'word',
+	});
+	
+	var self = this;	//http://stackoverflow.com/questions/20279484/how-to-access-the-correct-this-context-inside-a-callback#
+	this.limitSize = 10;
+	this.page = 1;
+	this.bindSearch = function (){
+		//keyword, offset, limitSize
+		var promise = searchService.search(self.searchString, 
+				(self.page-1) * self.limitSize ,
+				self.limitSize);
+
+		promise.then(function(data){$window.alert(self.searchString + ':done:' + data);
+				self.collection = [{
+					title : 'success1',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				},
+				{
+					title : 'success2',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				},
+				{
+					title : 'success3',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				}];
+		},
+				function(errMsg){$window.alert(self.searchString + ':fail:' + errMsg);
+
+				self.collection = [{
+					title : 'fail1',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				},
+				{
+					title : 'fail2',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				},{
+					title : 'fail3',
+					date : '200901010',
+					description : 'desc',
+					link : 'link'
+				}];
+		},
+				function(notiMsg){$window.alert(self.searchString + ':noti:' + notiMsg);});
+	};
 	this.collection = [{
-		title : 'hello',
+		title : 'test',
 		date : '200901010',
-		description : 'desc',
-		link : 'link'
-	}, {
-		title : 'hello1',
-		date : '200901010',
-		description : 'longlongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  onglongtextlonglongtext  ',
-		link : 'link'
-	}, {
-		title : 'hello2',
-		date : '200901010',
-		description : 'desc2',
-		link : 'link'
-	}, {
-		title : 'hello3',
-		date : '200901010',
-		description : 'desc3',
+		description : 'dasdfasfd asdfasdf safa sdfasdf asfasfasdfasdf asdfasdfasdfasdfasdfasdfasdfasdfasdfasdfhrthdth asdfasdfasdfhrthdthdrh drhdtrhdrthrdthrtr dsesc',
 		link : 'link'
 	}];
-
 }]);
 
 // $scope.collection = 
--- src/main/webapp/staticfiles/js/searchService.js
+++ src/main/webapp/staticfiles/js/searchService.js
@@ -4,12 +4,42 @@
 
 var app = angular.module('searchServiceModule', []);
 
-app.factory('searchService', function() {
+app.factory('searchService', [ '$http', '$q', function($http, $q) {
 	var search = function(keyword, offset, limitSize) {
-		//ajax $http
+		var deferred = $q.defer();
+		
+		// ajax $http
+		var headers = {
+			'Accept' : 'application/json',
+			'Content-Type' : 'application/json'
+		};
+		var params = {
+			'keyword' : keyword,
+			'offset' : offset,
+			'limitSize' : limitSize,
+			'mode' : 'DefaultRankAppliedToEachKeyword'
+		};
+		var config = {
+			'params' : params,
+			'headers' : headers
+		};
+
+		$http.get(url = '/search', config = config).then(function(response) {
+			if (response.status == 200) {
+				deferred.resolve(response.data);
+			} else {
+				deferred.reject('search fail');
+			}
+		}, function(response) {
+			deferred.reject(response.data);
+		}, function(response) {
+			deferred.reject(response.data);
+		});
+		return deferred.promise;
+		// then(successCallback, errorCallback, notifyCallback)
 	};
 
 	return {
 		search : search
 	};
-}); 
+} ]);
